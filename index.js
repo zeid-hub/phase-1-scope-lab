7797c2780a2d19ff665b5983f3881503fb6ba163
zeid <zeidhassan4@gmail.com>
2023-12-15 17:04:46 +0300

    Compited assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    customerName = 'BOB'
+}
+function setBestCustomer(){
+   bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'Assignment to constant variable.'
+function changeLeastFavoriteCustomer(){
+    throw leastFavoriteCustomer
+}
